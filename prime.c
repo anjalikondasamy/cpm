ec3e322cdc0fc40069cc2b80045d3b1575535f40
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-07 16:06:34 +0530

    cpgm
    good

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n Enter the number:");
+scanf("%d",&n);
+
+if((a%a)&&(a%1))
+printf("\n yes");
+else
+{
+printf("\n no");
+}
+return 0;
+}
